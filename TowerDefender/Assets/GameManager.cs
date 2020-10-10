2e4bec16924d1d92b84b1e28052abd3260d2cc01
Juan Marquez <juanm170130@gmail.com>
2020-10-10 00:32:53 -0500

    Added animations to the Game Over Display. Added a game manager which will control the flow of the game. Added a GameOver script. Added WaveSpawner script.

---
+++ TowerDefender/Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private bool gameEnded = false;
+    public GameObject gameOverUI;
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if(gameEnded)
+        {
+            return;
+        }
+
+        /*
+        if(PlayerStats.Lives <= 0)
+        {
+            EndGame();
+        }
+        */
+    }
+
+    public void EndGame()
+    {
+        gameEnded = true;
+        gameOverUI.SetActive(true);  
+    }
+}
