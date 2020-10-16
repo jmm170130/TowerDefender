eae1a0c72edc3cecad3405d72c46f9822f0535f3
poecilea <sarahlconaway@gmail.com>
2020-10-15 21:09:27 -0500

    Implemented wave spawner
    also made a folder for scripts and moved the scripts into it

---
+++ TowerDefender/Assets/Scripts/CameraController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float cameraSpeed = 30f;
+    public float minY = 15f, maxY = 80f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move camera up
+        if( Input.GetKey("w") )
+        {
+            transform.Translate( Vector3.forward * cameraSpeed * Time.deltaTime, Space.World);
+        }
+        // Move camera down
+        if( Input.GetKey("s") )
+        {
+            transform.Translate( Vector3.back * cameraSpeed * Time.deltaTime, Space.World);
+        }
+        // Move camera to the left
+        if( Input.GetKey("a") )
+        {
+            transform.Translate( Vector3.left * cameraSpeed * Time.deltaTime, Space.World);
+        }
+        // move camera to the right
+        if( Input.GetKey("d") )
+        {
+            transform.Translate( Vector3.right * cameraSpeed * Time.deltaTime, Space.World);
+        }
+
+        // Zoom in
+        if( Input.GetKey("i") )
+        {
+            Vector3 viewPos =  transform.position;
+            if( viewPos.y > minY)
+            {
+                transform.Translate( Vector3.down * cameraSpeed * Time.deltaTime, Space.World);
+            }
+        }
+
+        // Zoom out 
+        if( Input.GetKey("o") )
+        {
+            Vector3 viewPos =  transform.position;
+            if( viewPos.y < maxY)
+            {
+                transform.Translate( Vector3.up * cameraSpeed * Time.deltaTime, Space.World);
+            }
+        }
+    }
+}
---
+++ TowerDefender/Assets/Scripts/GameOver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public Text levelsText;
+
+    /*
+    void onEnable()
+    {
+         levelsText.text = PlayerStats.Rounds.ToString();
+    }
+    */
+
+    public void Retry()
+    {
+        SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex );
+    }
+
+    public void Menu()
+    {
+        Debug.Log("In Menu method");
+    }
+}
