8c11366f90c308df31b9efb4339691239b4994b0
Juan Marquez <juanm170130@gmail.com>
2020-11-09 22:04:14 -0600

    Implemented the weapon shop

--- TowerDefender/Assets/Scripts/BuildManager.cs
+++ TowerDefender/Assets/Scripts/BuildManager.cs
@@ -4,29 +4,26 @@ using UnityEngine;
 
 public class BuildManager : MonoBehaviour
 {
-    public static BuildManager buildManager;
-    public GameObject turretPrefab;
-    private GameObject turretToBuild;
-
-    public GameObject GetTurretToBuild()
-    {
-        return turretToBuild;
-    }
-
+    public static BuildManager instance;
+  
+    // Maintain a single BuildManager instance
     void Awake()
     {
-        buildManager = this;
+        instance = this;
+    }
+   
+    public GameObject GetItemToBuild()
+    {
+        return itemToBuild;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        turretToBuild = turretPrefab;
-    }
+    public GameObject turretPrefab;
+    public GameObject laserPrefab;
+    public GameObject missileLauncherPrefab;
 
-    // Update is called once per frame
-    void Update()
+    private GameObject itemToBuild;
+    public void SetItemToBuild( GameObject item)
     {
-        
+        itemToBuild = item;
     }
 }
--- TowerDefender/Assets/Scripts/Node.cs
+++ TowerDefender/Assets/Scripts/Node.cs
@@ -1,36 +1,59 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Node : MonoBehaviour
 {
-    public Color hoverColor;
     public Vector3 offset;
 
-    private GameObject turret;
-
     private Color startColor;
+    public Color hoverColor;
+
     private Renderer render;
+    BuildManager buildManager;
 
     void Start()
     {
         render = GetComponent<Renderer>();
         startColor = render.material.color;
+        buildManager = BuildManager.instance;
     }
 
+    private GameObject weapon;
     void OnMouseDown()
     {
-        if(turret != null)
+        if(EventSystem.current.IsPointerOverGameObject())
+		{
+			return;
+		}
+
+		if(buildManager.GetItemToBuild() == null)
+		{
+			return;
+		}
+
+        if(weapon != null)
         {
             return;
         }
 
-        //Build the turret
-        GameObject turretToBuild = BuildManager.buildManager.GetTurretToBuild();
-        turret = (GameObject)Instantiate(turretToBuild, transform.position + offset, transform.rotation);
+        //Build the weapon
+        GameObject itemToBuild = buildManager.GetItemToBuild();
+        weapon = (GameObject)Instantiate(itemToBuild, transform.position + offset, transform.rotation);
     }
 
+    // Highligth the tile the mouse is hovering over
     void OnMouseEnter()
     {
+        if(EventSystem.current.IsPointerOverGameObject())
+		{
+			return;
+		}
+
+		if(buildManager.GetItemToBuild() == null)
+		{
+			return;
+		}
         render.material.color = hoverColor;
     }
 
